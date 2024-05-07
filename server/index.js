f5094ee00f0037be74ee54ceb7ab4265dd224109
coungdev <coungdev@gmail.com>
2024-05-07 19:07:42 +0700

    .

--- server/index.js
+++ server/index.js
@@ -19,6 +19,9 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.use("/api/user", userRoute);
 app.use("/api/user", residencyRouter);
